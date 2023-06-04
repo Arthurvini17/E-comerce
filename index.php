a4f271697fb2c07fd11e9dd8059bd46daf7dba25
arthur <arthurdedsec@gmail.com>
2023-06-04 17:45:25 -0300

    enviar

--- index.php
+++ index.php
@@ -1,33 +1,57 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <title>E-SHOP/title>
+  <title>Minha Loja de Eletrônicos</title>
 </head>
 <body>
-  <div class="container">
-    <h1>E-SHOP</h1>
-    <section>
-      <h2>Cardápio</h2>
-      <p>Aqui você encontra os melhores eletronicos e com os melhores preços</p>
-      <!-- Adicione o conteúdo do cardápio aqui -->
-    </section>
-  <?php 
-  echo "text";
-  ?>
-    <section>
-      <h2>Sobre nós</h2>
-      <p>Conheça nossa história</p>
-      <!-- Adicione o conteúdo sobre o restaurante aqui -->
-    </section>
+  <header>
+    <!-- Seção do cabeçalho da loja -->
+    <h1>Minha Loja de Eletrônicos</h1>
+    <nav>
+      <!-- Navegação do site, como menu e links -->
+      <ul>
+        <li><a href="#">Página Inicial</a></li>
+        <li><a href="#">Produtos</a></li>
+        <li><a href="#">Carrinho</a></li>
+        <li><a href="#">Sobre Nós</a></li>
+        <li><a href="#">Contato</a></li>
+      </ul>
+    </nav>
+  </header>
 
-    <footer>
+  <section id="destaques">
+    <!-- Seção de destaques ou produtos em destaque -->
+    <h2>Destaques</h2>
+    <!-- Conteúdo dos destaques aqui -->
+  </section>
+
+  <section id="produtos">
+    <!-- Seção de listagem de produtos -->
+    <h2>Produtos</h2>
+    <!-- Conteúdo dos produtos aqui -->
+  </section>
+
+  <section id="carrinho">
+    <!-- Seção do carrinho de compras -->
+    <h2>Carrinho</h2>
+    <!-- Conteúdo do carrinho aqui -->
+  </section>
+
+  <section id="sobre-nos">
+    <!-- Seção "Sobre Nós" -->
+    <h2>Sobre Nós</h2>
+    <!-- Conteúdo sobre a empresa aqui -->
+  </section>
+
+  <section id="contato">
+    <!-- Seção de contato -->
     <h2>Contato</h2>
-      <p>Entre em contato conosco para fazer sua compra ou tirar suas dúvidas.</p>
-      <!-- Adicione o formulário de contato aqui -->
-    </footer>
-  </div>
+    <!-- Formulário de contato ou informações de contato aqui -->
+  </section>
+
+  <footer>
+    <!-- Rodapé da página -->
+    <p>&copy; 2023 Minha Loja de Eletrônicos. Todos os direitos reservados.</p>
+  </footer>
 </body>
 </html>
