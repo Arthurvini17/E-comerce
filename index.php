c97936943ca1c71787887cb4cb84fed30d575bcd
arthur <arthurdedsec@gmail.com>
2023-06-04 17:41:26 -0300

    enviar

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
-<?php 
-echo "teste";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>E-SHOP/title>
+</head>
+<body>
+  <div class="container">
+    <h1>E-SHOP</h1>
+    <section>
+      <h2>Cardápio</h2>
+      <p>Aqui você encontra os melhores eletronicos e com os melhores preços</p>
+      <!-- Adicione o conteúdo do cardápio aqui -->
+    </section>
+  <?php 
+  echo "text";
+  ?>
+    <section>
+      <h2>Sobre nós</h2>
+      <p>Conheça nossa história</p>
+      <!-- Adicione o conteúdo sobre o restaurante aqui -->
+    </section>
+
+    <footer>
+    <h2>Contato</h2>
+      <p>Entre em contato conosco para fazer sua compra ou tirar suas dúvidas.</p>
+      <!-- Adicione o formulário de contato aqui -->
+    </footer>
+  </div>
+</body>
+</html>
