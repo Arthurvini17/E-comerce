45f6b4485e454c8e34d446895692cb360419eb45
arthur <arthurdedsec@gmail.com>
2023-06-05 12:08:56 -0300

    enviado

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style.css">
   <title>Document</title>
 </head>
+
 </html>
+
 <body>
   <header>
     <!-- Seção do cabeçalho da loja -->
@@ -26,23 +29,41 @@
 
   <section id="ofertas">
     <!-- Seção de destaques ou produtos em destaque -->
-    <h2>Ofertas</h2>
+    <h2>OFERTAS</h2>
     <div class="container">
-    <div class="monitor">
-    <p>Monitor 244HZ por apenas <span>1000 R$</span></p>
-    <img src="/eletronicos/monitor.png" alt="monitor">
+      <div class="monitor">
+        
+        <img src="/eletronicos/monitor.png" alt="monitor" class="monitor-1">
+        <h4>Monitor 244HZ por apenas <span>1000 R$</span></h4>
+        <p>Coisas sobre o produto aqui<p>
+      </div>
+      <div class="mouse">
+        <img src="/eletronicos/mouse-preto.png" alt="mouse-preto" class="mouse-preto">
+        <h4>Mouse Gamer 1000DPI por apenas <span>120 R$<span></h4>
+        <p>Coisas sobre o produto aqui<p>
+      </div>
+        <div class="fone">
+          <img src="/eletronicos/fone.png" alt="fone" class="fone.img"> 
+          <h4>Fone redragon <span> 130 R$<span></h4>
+          <p>Coisas sobre o produto aqui<p>
+        </div>
+        <div class="controle">
+          <img src="/eletronicos/controle.png" alt="" class="controle-xbox">
+          <h4>Controle XBOX ONE S Branco <span>230 R$</span></h4>
+          <p>Coisas sobre o produto aqui<p>
+            
+        </div>
     </div>
-    </div>
-   
-        <!-- Conteúdo dos destaques aqui -->
+
+    <!-- Conteúdo dos destaques aqui -->
   </section>
 
   <section id="produtos">
-  <h2>Produtos</h2>
+    <h2>Produtos</h2>
     <img src="/eletronicos/Picsart_23-06-05_01-23-53-024.png" alt="pc-branco" class="pc-branco">
     <img src="/eletronicos/714sFk3poYL._AC_UL320_-removebg-preview.png" alt="pc-preto" class="pc-preto">
     <img src="/eletronicos/61a30YVcP2L._AC_UL320_-removebg-preview.png" alt="pc">
-    
+
     <!-- Conteúdo dos produtos aqui -->
   </section>
 
@@ -69,4 +90,5 @@
     <p>&copy; 2023 Minha Loja de Eletrônicos. Todos os direitos reservados.</p>
   </footer>
 </body>
+
 </html>
